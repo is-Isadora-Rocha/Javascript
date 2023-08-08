88309fa9a78839cc03db3f5f87ecc5d13bda3073
is-Isadora-Rocha <is.isadora.rocha@gmail..com>
2023-08-08 09:23:49 -0300

    tipos de variaveis

---
+++ variaveis.js
@@ -0,0 +1,28 @@
+// VARIÁVEIS
+
+let idade = 26;
+console.log(idade)
+
+//TIPOS DE VARIÁVEIS
+
+idade = 30;
+let numero = 30;
+let altura = 1.77;
+let nome = "Isadora"
+let estudando = true 
+
+console.log(numero, typeof numero)
+console.log(altura, typeof altura)
+console.log(nome, typeof nome)
+console.log(estudando, typeof estudando)
+
+let outra; 
+console.log(outra) //undefind
+
+let nula = null; 
+console.log(nula)
+
+// var = let (sintaxe antiga, 2015)
+
+const constante = 100; //valor inalterável 
+
