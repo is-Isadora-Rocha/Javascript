7be36e1c446b85ce3faf0d8af08b21b154f5e5c9
is-Isadora-Rocha <is.isadora.rocha@gmail..com>
2023-08-09 09:43:08 -0300

    operadores aritmeticos Math

--- operadores_aritmeticos.js
+++ operadores_aritmeticos.js
@@ -3,4 +3,19 @@ let segundoNumero = 2
 
 const soma = primeiroNumero + segundoNumero;
 console.log('Soma =', soma)
+console.log('Subtração =', primeiroNumero - segundoNumero)
+console.log('Multiplicação =', primeiroNumero * segundoNumero)
+console.log('Divisão =', primeiroNumero / segundoNumero)
+console.log('Exponenciação =', primeiroNumero ** segundoNumero)
+console.log('Resto da divisão =', primeiroNumero % segundoNumero)
 
+// Precedência de Operadores
+console.log(3 + 7 * 3)
+console.log((3 + 7) * 3)
+console.log((10 + 7 + 8) / 3)
+
+// Biblioteca Math
+console.log(Math.PI)
+console.log(Math.sqrt(16)) // raiz quadrada
+
+//MDN => documentação do Javascript
